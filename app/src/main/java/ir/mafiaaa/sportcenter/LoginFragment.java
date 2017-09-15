e2d84a3f77c321c6a935ced192d41106feeaf6f1
Amir Reza <arg7971@gmail.com>
2017-09-15 19:52:28 +0430

    fixed login and splashScreen transition

--- app/src/main/java/ir/mafiaaa/sportcenter/LoginFragment.java
+++ app/src/main/java/ir/mafiaaa/sportcenter/LoginFragment.java
@@ -25,8 +25,7 @@ import com.backtory.java.internal.LoginResponse;
 
 public class LoginFragment extends Fragment {
 
-    private void log(String s)
-    {
+    private void log(String s) {
         Log.i("NoEmulators", s);
     }
 
@@ -40,11 +39,11 @@ public class LoginFragment extends Fragment {
     Button btnLogin;
 
 
-    String password , emailAddress;
+    String password, emailAddress;
 
     // TODO: Rename and change types of parameters
 
-    public static void setCurrentActivity(Activity activity){
+    public static void setCurrentActivity(Activity activity) {
 
         act = activity;
     }
@@ -62,16 +61,16 @@ public class LoginFragment extends Fragment {
         relativeLayout = (RelativeLayout) rootView.findViewById(R.id.LoginFragment);
         btnLogin = (Button) rootView.findViewById(R.id.btn_Login);
 
-        email.setOnFocusChangeListener(new View.OnFocusChangeListener(){
+        email.setOnFocusChangeListener(new View.OnFocusChangeListener() {
 
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
-                if (email.getText().toString().isEmpty()){
+                if (email.getText().toString().isEmpty()) {
 
                     emailLayout.setErrorEnabled(true);
                     emailLayout.setError("ایمیل خود را وارد کنید");
 
-                }else {
+                } else {
                     emailLayout.setErrorEnabled(false);
                 }
 
@@ -80,8 +79,7 @@ public class LoginFragment extends Fragment {
 
         });
 
-        if (BacktoryUser.getCurrentUser() != null)
-        {
+        if (BacktoryUser.getCurrentUser() != null) {
             log(BacktoryUser.getCurrentUser().getUserId());
         }
 
@@ -94,12 +92,12 @@ public class LoginFragment extends Fragment {
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
 
-                if (email.getText().toString().isEmpty()){
+                if (email.getText().toString().isEmpty()) {
 
                     emailLayout.setErrorEnabled(true);
                     emailLayout.setError("ایمیل خود را وارد کنید");
 
-                }else {
+                } else {
                     emailLayout.setErrorEnabled(false);
                 }
 
@@ -115,25 +113,19 @@ public class LoginFragment extends Fragment {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (initStrings())
-                {
+                if (initStrings()) {
                     BacktoryUser.loginInBackground(emailAddress, SecurityManager.hash_to_SHA1(password), new BacktoryCallBack<LoginResponse>() {
                         @Override
                         public void onResponse(BacktoryResponse<LoginResponse> response) {
-                            if (response.isSuccessful())
-                            {
+                            if (response.isSuccessful()) {
                                 btnLogin.setEnabled(false);
-                                Toast.makeText(getContext() , BacktoryUser.getCurrentUser().getFirstName() + "ورود موفقیت امیز بود ، خوش امدی " , Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(getActivity() , Main.class);
-                                startActivity(intent);
-                            }
-                            else if(response.code() == HttpStatusCode.Unauthorized.code())
-                            {
-                                Toast.makeText(getContext() , "ایمیل و یا رمز عبور اشتباه است" , Toast.LENGTH_SHORT).show();
-                            }
-                            else
-                            {
-                                Toast.makeText(getContext() , "مشکلی پیش امده، اتصال ب اینترنت و یا تایید ایمیل میتوانند باعث این مشکل باشند" , Toast.LENGTH_SHORT).show();
+                                Toast.makeText(getContext(), BacktoryUser.getCurrentUser().getFirstName() + "ورود موفقیت امیز بود ، خوش امدی ", Toast.LENGTH_SHORT).show();
+                                startNextActivity();
+
+                            } else if (response.code() == HttpStatusCode.Unauthorized.code()) {
+                                Toast.makeText(getContext(), "ایمیل و یا رمز عبور اشتباه است و یا نیاز به تایید ایمیل دارید", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(getContext(), "مشکلی پیش امده، اتصال ب اینترنت و یا تایید ایمیل میتوانند باعث این مشکل باشند", Toast.LENGTH_SHORT).show();
                             }
                         }
                     });
@@ -146,15 +138,19 @@ public class LoginFragment extends Fragment {
 
     }
 
-    private boolean initStrings()
-    {
+    private void startNextActivity() {
+        Intent intent = new Intent(getActivity(), Main.class);
+        startActivity(intent);
+        getActivity().finish();
+    }
+
+    private boolean initStrings() {
         password = pass.getText().toString();
         emailAddress = email.getText().toString();
 
 
-        if (emailAddress.equals("") || password.equals(""))
-        {
-            Toast.makeText(getContext() ,"تمامی فیلد هارو پر کنید" , Toast.LENGTH_SHORT).show();
+        if (emailAddress.equals("") || password.equals("")) {
+            Toast.makeText(getContext(), "تمامی فیلد هارو پر کنید", Toast.LENGTH_SHORT).show();
             return false;
         }
         return true;
