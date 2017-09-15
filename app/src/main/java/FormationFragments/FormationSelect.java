244018ce15aedfd95576a34b70be0e1b191c8654
zsezs <alireza1376@gmail.com>
2017-09-15 16:32:52 +0430

    hi how are you?

--- app/src/main/java/FormationFragments/FormationSelect.java
+++ app/src/main/java/FormationFragments/FormationSelect.java
@@ -5,16 +5,26 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import ir.mafiaaa.sportcenter.R;
+import ir.mafiaaa.sportcenter.RecyclerAdapter;
+
 
 
 public class FormationSelect extends Fragment {
 
     private OnFragmentInteractionListener mListener;
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private List<ListItemFormation> listItems;
 
     public FormationSelect() {
         // Required empty public constructor
@@ -25,7 +35,29 @@ public class FormationSelect extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_formation_select, container, false);
+        final View view = inflater.inflate(R.layout.fragment_formation_select, container, false);
+
+        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView_formation);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        listItems = new ArrayList<>();
+
+        for(int i =0;i<=10; i++ ){
+
+            ListItemFormation listItem = new ListItemFormation(
+                    "heading" + (i+1),
+                    "Loren ipsum dummy text"
+            );
+
+            listItems.add(listItem);
+        }
+
+        adapter = new RecyclerFormationAdapter(listItems,getContext());
+
+        recyclerView.setAdapter(adapter);
+
+        return view;
     }
 
     @Override
