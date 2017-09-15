850e5db1d036c547efe42402324d819405c62263
AssassinViper <so.mgfiotygf@gmail.com>
2017-09-15 08:50:49 +0430

    amp2

---
+++ app/src/main/java/FormationFragments/Formation433.java
@@ -0,0 +1,30 @@
+package FormationFragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ir.mafiaaa.sportcenter.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Formation433 extends Fragment {
+
+
+    public Formation433() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_formation433, container, false);
+    }
+
+}
