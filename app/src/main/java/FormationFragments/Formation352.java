751aad9ac9e96a3784b98f3d94a44daa7f4389ca
AssassinViper <so.mgfiotygf@gmail.com>
2017-09-15 11:44:31 +0430

    amp4

--- app/src/main/java/FormationFragments/Formation352.java
+++ app/src/main/java/FormationFragments/Formation352.java
@@ -1,11 +1,13 @@
 package FormationFragments;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import ir.mafiaaa.sportcenter.R;
 
@@ -28,6 +30,79 @@ public class Formation352 extends Fragment {
         // Inflate the layout for this fragment
         log("6");
         return inflater.inflate(R.layout.fragment_formation352, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ImageView num1 = (ImageView) view.findViewById(R.id.num1);
+        ImageView num2 = (ImageView) view.findViewById(R.id.num2);
+        ImageView num3 = (ImageView) view.findViewById(R.id.num3);
+        ImageView num4 = (ImageView) view.findViewById(R.id.num4);
+        ImageView num5 = (ImageView) view.findViewById(R.id.num5);
+        ImageView num6 = (ImageView) view.findViewById(R.id.num6);
+        ImageView num7 = (ImageView) view.findViewById(R.id.num7);
+        ImageView num8 = (ImageView) view.findViewById(R.id.num8);
+        ImageView num9 = (ImageView) view.findViewById(R.id.num9);
+        ImageView num10 = (ImageView) view.findViewById(R.id.num10);
+        ImageView num11 = (ImageView) view.findViewById(R.id.num11);
+
+        View.OnClickListener clickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId())
+                {
+                    case R.id.num1:
+                        break;
+                    case R.id.num2:
+                    case R.id.num3:
+                    case R.id.num4:
+                        break;
+                    case R.id.num5:
+                    case R.id.num6:
+                    case R.id.num7:
+                    case R.id.num8:
+                    case R.id.num9:
+                        break;
+                    case R.id.num10:
+                    case R.id.num11:
+                        break;
+
+                }
+            }
+        };
+
+
+        num1.setOnClickListener(clickListener);
+        num2.setOnClickListener(clickListener);
+        num3.setOnClickListener(clickListener);
+        num4.setOnClickListener(clickListener);
+        num5.setOnClickListener(clickListener);
+        num6.setOnClickListener(clickListener);
+        num7.setOnClickListener(clickListener);
+        num8.setOnClickListener(clickListener);
+        num9.setOnClickListener(clickListener);
+        num10.setOnClickListener(clickListener);
+        num11.setOnClickListener(clickListener);
+    }
+
+
+    public void selectDarvazeBan(){
+
+    }
+
+    public void selectModafe(){
+
+    }
+
+    public void selectHafbak(){
+
+    }
+
+    public void selectMohajem(){
+
     }
 
 }
